d159af4708f1126e1afaf11d0285b161c6654a0b
angenlop <angenlop@amazon.com>
2026-04-15 08:47:52 -0700

    Update seal-comment-generator.user.js

--- seal-comment-generator.user.js
+++ seal-comment-generator.user.js
@@ -102,7 +102,7 @@
             if (rackAsset && sealNumber) {
                 const text = `Rack Asset#: ${rackAsset}
 Contains no customer data bearing drives and is sealed with Seal#: ${sealNumber}
-I bagged and securely sealed the rack with tamper evident seals according to standard and in preparation for RZ to RZ transfer to RRL.`;
+I bagged and securely sealed the rack with tamper evident seals according to standard and in preparation for RZ-to-RZ transfer to RRL.`;
                 copyToClipboard(text);
                 dialog.remove();
             } else {
@@ -115,9 +115,10 @@ I bagged and securely sealed the rack with tamper evident seals according to sta
             const sealNumber = sealInput.value;
             if (rackAsset && sealNumber) {
                 const text = `Rack Asset#: ${rackAsset}
+Sealed with Seal#: ${sealNumber}`;
 Rack has drives and is being shipped intact for RRL to process.
 Refer to step 11.1.1 of the Network SOP: https://policy.a2z.com/docs/59394/publication
-Sealed with Seal#: ${sealNumber}`;
+I bagged and securely sealed the rack with tamper-evident seals according to standard and in preparation for RZ-to-RZ transfer to RRL.
                 copyToClipboard(text);
                 dialog.remove();
             } else {
